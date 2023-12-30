96c791912ce8afc4b5151ce313bf3538c48c3129
Lucas Hunter <lucasbhunter@gmail.com>
2023-12-29 22:55:38 -0400

    fixed articles loading.

--- assets/js/load.js
+++ assets/js/load.js
@@ -11,55 +11,54 @@ function loadArticle(article) {
 }
 
 
-function loadArticles(articles){
+async function loadArticles(articles){
+    for (const article of articles) {
+        try {
+            // Fetch the content of the article page
+            const response = await fetch(article);
+            const content = await response.text();
 
-    articles.forEach(function(article) {
-        // Fetch the content of the article page
-            fetch(article)
-                .then(response => response.text())
-                .then(content => {
-                    // Create a DOM parser
-                    var parser = new DOMParser();
-        
-                    // Parse the content
-                    var doc = parser.parseFromString(content, 'text/html');
-        
-                    // Extract the title and the first paragraph of the article
-                    var title = doc.querySelector('h1').textContent;
-                    var image = doc.querySelector('img').src;
-                    var category = doc.querySelector('p').textContent;
-                    var paragraph = doc.querySelector('#description').textContent;
-                    //var author = doc.querySelector('#author').textContent;
-                    var date = doc.querySelector('#date').textContent;
+            // Create a DOM parser
+            var parser = new DOMParser();
 
-                 
+            // Parse the content
+            var doc = parser.parseFromString(content, 'text/html');
 
-                    // Create a card with the title and the paragraph
-                    var card = `
-                        <div class="row mb-2 ml-2 mr-2">
-                            <div class="customCard">
-                            <img class="customCard-image" src="${image}">
-                                <div class="pl-2 pl-md-3">
-                                    <h2 class="mb-2 h6 font-weight-bold">
-                                        <a class="text-dark" href="${article}">${title}</a>
-                                        <small class="text-danger">${category}</small>
-                                    </h2>
-                                   
-                                    <div class="card-text text-muted small">
-                                        ${paragraph}
-                                    </div>
-                                   
+            // Extract the title and the first paragraph of the article
+            var title = doc.querySelector('h1').textContent;
+            var image = doc.querySelector('img').src;
+            var category = doc.querySelector('p').textContent;
+            var paragraph = doc.querySelector('#description').textContent;
+            //var author = doc.querySelector('#author').textContent;
+            var date = doc.querySelector('#date').textContent;
 
-                                    <small class="text-muted">${date} &middot; 5 min read</small>
-                                </div>
+            // Create a card with the title and the paragraph
+            var card = `
+                <div class="row mb-2 ml-2 mr-2">
+                    <div class="customCard">
+                    <img class="customCard-image" src="${image}">
+                        <div class="pl-2 pl-md-3">
+                            <h2 class="mb-2 h6 font-weight-bold">
+                                <a class="text-dark" href="${article}">${title}</a>
+                                <small class="text-danger">${category}</small>
+                            </h2>
+                           
+                            <div class="card-text text-muted small">
+                                ${paragraph}
                             </div>
+                           
+
+                            <small class="text-muted">${date} &middot; 5 min read</small>
                         </div>
-                    `;
-        
-                    // Insert the card into the container
-                    document.getElementById('articleCards').innerHTML += card;
-                })
-                .catch(error => console.error('Error:', error));
-        });
+                    </div>
+                </div>
+            `;
+
+            // Insert the card into the container
+            document.getElementById('articleCards').innerHTML += card;
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
 }
 
